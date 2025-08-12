8f63e4a42602550150c86a1a2f3d44c8a8723521
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2025-08-12 11:41:26 +0200

    Add bootstrap link to ui project

--- src/UI/azuredemoui/src/app/app.component.ts
+++ src/UI/azuredemoui/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SendJsonComponent } from './features/send-json/component/send-json.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    RouterOutlet,
+    SendJsonComponent
+  ],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'azuredemoui';
