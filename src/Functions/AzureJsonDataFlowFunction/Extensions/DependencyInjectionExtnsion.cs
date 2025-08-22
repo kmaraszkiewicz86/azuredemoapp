29f65e7c23c128d1ccf6353adfae56a39220fac9
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2025-08-22 16:03:56 +0200

    • Create DependencyInjection extension class to register services and required Azure clients

---
+++ src/Functions/AzureJsonDataFlowFunction/Extensions/DependencyInjectionExtnsion.cs
@@ -0,0 +1,81 @@
+using Azure.Storage.Blobs;
+using AzureJsonDataFlowFunction.Services;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace AzureJsonDataFlowFunction.Extensions
+{
+    public static class DependencyInjectionExtnsion
+    {
+        public static IServiceCollection RegisterAllServices(this IServiceCollection services, Assembly? assembly = null)
+        {
+            RegisterAllByMarkerInterface<IService>(services, assembly);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Registers BlobServiceClient and CosmosClient in DI.
+        /// </summary>
+        public static IServiceCollection RegisterExternalAzureClients(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton(x =>
+            {
+                var connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+                return new BlobServiceClient(connectionString);
+            });
+
+            services.AddSingleton<CosmosClient>(serviceProvider =>
+            {
+                var config = serviceProvider.GetRequiredService<IConfiguration>();
+                string endpointUri = config["CosmosDb:Endpoint"]!;
+                string primaryKey = config["CosmosDb:PrimaryKey"]!;
+                return new CosmosClient(endpointUri, primaryKey);
+            });
+
+            return services;
+        }
+
+        /// <summary>
+        /// Generic method that registers all interfaces inheriting from the marker interface and their implementations.
+        /// </summary>
+        private static void RegisterAllByMarkerInterface<TMarker>(
+            IServiceCollection services,
+            Assembly? assembly = null,
+            ServiceLifetime lifetime = ServiceLifetime.Scoped)
+            where TMarker : class
+        {
+            assembly ??= Assembly.GetExecutingAssembly();
+
+            var serviceTypes = assembly.GetTypes()
+                .Where(t => t.IsInterface && typeof(TMarker).IsAssignableFrom(t) && t != typeof(TMarker));
+
+            foreach (var serviceType in serviceTypes)
+            {
+                var implementationType = assembly.GetTypes()
+                    .FirstOrDefault(c =>
+                        c.IsClass &&
+                        !c.IsAbstract &&
+                        serviceType.IsAssignableFrom(c));
+
+                if (implementationType != null)
+                {
+                    switch (lifetime)
+                    {
+                        case ServiceLifetime.Singleton:
+                            services.AddSingleton(serviceType, implementationType);
+                            break;
+                        case ServiceLifetime.Transient:
+                            services.AddTransient(serviceType, implementationType);
+                            break;
+                        default:
+                            services.AddScoped(serviceType, implementationType);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
--- src/Functions/AzureJsonDataFlowFunction/Program.cs
+++ src/Functions/AzureJsonDataFlowFunction/Program.cs
@@ -1,27 +1,13 @@
-using Azure.Storage.Blobs;
-using Microsoft.Azure.Cosmos;
+using AzureJsonDataFlowFunction.Extensions;
 using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-builder.Services.AddSingleton(x =>
-{
-    var connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-    return new BlobServiceClient(connectionString);
-});
-
-builder.Services.AddSingleton<CosmosClient>(serviceProvider =>
-{
-    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-    string endpointUri = configuration["CosmosDb:Endpoint"]!;
-    string primaryKey = configuration["CosmosDb:PrimaryKey"]!;
-
-    return new CosmosClient(endpointUri, primaryKey);
-});
+builder.Services
+    .RegisterExternalAzureClients(builder.Configuration)
+    .RegisterAllServices();
 
 builder.Build().Run();
