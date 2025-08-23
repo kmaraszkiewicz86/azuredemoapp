b103e4810b9338486cb48fc8cde0bbbee74799ac
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2025-08-23 14:28:44 +0200

    Add configuration for production and local API endpoints in UI project

--- src/UI/azuredemoui/src/app/environments/environment.ts
+++ src/UI/azuredemoui/src/app/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
   azureFunctionToken: 'token',
-  azureFunctionUrl: 'http://localhost:7144/api'
+  // Local Azure Function URL
+  //azureFunctionUrl: 'http://localhost:7144/api',
+  // Deployed Azure Function URL
+  azureFunctionUrl: 'https://azuredemokmar-func-d4fdf0ccg6g9d3ar.polandcentral-01.azurewebsites.net/api/jsonfiles?'
 };
