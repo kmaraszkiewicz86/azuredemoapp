42265f67aa8c4eb880a41af16dd5bb003576f4c0
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2025-08-25 15:52:50 +0200

    Add interceptor classes to app configuration file

--- src/UI/azuredemoui/src/app/app.config.ts
+++ src/UI/azuredemoui/src/app/app.config.ts
@@ -6,10 +6,13 @@ import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AzureFunctionTokenInterceptor } from './interceptors/azure-function-token-interceptor';
 import { combineReducers } from '@ngrx/store';
 import { sendJsonFeatureReducers } from './features/send-json/store/send-json.reducer';
 import { SendJsonEffects } from './features/send-json/store/effects/send-json.send.effects';
 import { GetJsonEffects } from './features/send-json/store/effects/send-json.get.effects';
+import { GlobalErrorInterceptor } from './interceptors/global-error-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,6 +23,16 @@ export const appConfig: ApplicationConfig = {
     }),
     provideEffects(SendJsonEffects, GetJsonEffects),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AzureFunctionTokenInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: GlobalErrorInterceptor,
+      multi: true
+    }
   ]
 };
--- src/UI/azuredemoui/src/app/environments/environment.ts
+++ src/UI/azuredemoui/src/app/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   // Local Azure Function URL
   //azureFunctionUrl: 'http://localhost:7144/api',
   // Deployed Azure Function URL
-  azureFunctionUrl: 'https://azuredemokmar-func-d4fdf0ccg6g9d3ar.polandcentral-01.azurewebsites.net/api/jsonfiles?'
+  azureFunctionUrl: 'https://azuredemokmar-func-d4fdf0ccg6g9d3ar.polandcentral-01.azurewebsites.net/api'
 };
