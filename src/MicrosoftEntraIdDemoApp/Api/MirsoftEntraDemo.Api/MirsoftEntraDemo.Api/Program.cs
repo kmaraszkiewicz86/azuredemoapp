2516ef2acd44a87e779a72ac9ea0e70f00496f7d
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2026-03-09 17:09:58 +0100

    Add Microsoft Entra ID authentication with SimpleCqrs CQRS pattern and Vertical Slice Architecture

--- src/MicrosoftEntraIdDemoApp/Api/MirsoftEntraDemo.Api/MirsoftEntraDemo.Api/Program.cs
+++ src/MicrosoftEntraIdDemoApp/Api/MirsoftEntraDemo.Api/MirsoftEntraDemo.Api/Program.cs
@@ -1,8 +1,22 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using MirsoftEntraDemo.Api.Features;
+using SimpleCqrs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Konfiguracja SimpleCqrs
+builder.Services.ConfigureSimpleCqrs(typeof(Program).Assembly);
+
+// Konfiguracja Microsoft Entra ID
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -16,28 +30,10 @@ app.UseSwaggerUI(options =>
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Rejestracja wszystkich feature endpoints
+app.MapFeatureEndpoints();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
