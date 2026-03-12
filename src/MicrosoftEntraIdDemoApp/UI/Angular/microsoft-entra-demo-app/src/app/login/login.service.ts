eeac190e25abeb2b1adb4c9d95738e02e1f21924
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2026-03-12 13:15:19 +0100

    Add the api url to env file for angular project and update workflow for angular app to deploy app with expected api url parameter

--- src/MicrosoftEntraIdDemoApp/UI/Angular/microsoft-entra-demo-app/src/app/login/login.service.ts
+++ src/MicrosoftEntraIdDemoApp/UI/Angular/microsoft-entra-demo-app/src/app/login/login.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class LoginService {
   login(): void {
-    window.location.href = 'https://localhost:7161/login';
+    window.location.href = `${environment.apiHost}/login`;
   }
 }
