011b9de6bb19991be63185ba59f3c3468acf5d4a
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2026-05-10 21:57:58 +0200

    Changed logic for the page's load event from code-behind to behaviors for MVVM.

--- src/MicrosoftEntraIdDemoApp/UI/DotnetMaui/MicrosoftEntraIdDemoApp.Logic/Features/Login/LoginPage.xaml.cs
+++ src/MicrosoftEntraIdDemoApp/UI/DotnetMaui/MicrosoftEntraIdDemoApp.Logic/Features/Login/LoginPage.xaml.cs
@@ -9,13 +9,4 @@ public partial class LoginPage : ContentPage
 		InitializeComponent();
 		BindingContext = viewModel;
 	}
-
-    //it is created temporialy before I update the Kmaraszkiewicz86.Maui.Behaviors to support .net 10.0
-    private void ContentPage_Appearing(object sender, EventArgs e)
-    {
-        if (BindingContext is LoginViewModel viewModel)
-        {
-            viewModel.LoadCommand.Execute(null);
-        }
-    }
 }
